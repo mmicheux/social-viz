a4c5e227dadabcebced87b7b76dd3bb078984a43
Molly Micheux <mmicheux@gmail.com>
2022-08-04 19:16:04 +0200

    Update fe to display api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [backendData, setBackendData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => {
+        setBackendData(data)
+      })
+  }, []);
+
+  if (!backendData) return;
+
+  const { tweets } = backendData;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {tweets.map(tweet => (<div key={tweet.id} > {tweet.title}</div>))
+      }
+    </div >
   );
 }
 
